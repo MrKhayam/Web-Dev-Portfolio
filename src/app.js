a658979985bdf781938e79eba0f521dcc3fbe714
Khayam Ijaz <ijazkhayam@gmail.com>
2024-07-27 11:09:21 +0500

    update

--- src/app.js
+++ src/app.js
@@ -6,12 +6,12 @@ const inpMsg = document.querySelector('.inpMsg');
 
 
     const phoneNumber = '03175080524';
-    const message = encodeURIComponent(inpMsg.val);
-    const name = encodeURIComponent(inpName.val);
-    const email = encodeURIComponent(inpMail.val);
+    const message = encodeURIComponent(inpMsg.value);
+    const name = encodeURIComponent(inpName.value);
+    const email = encodeURIComponent(inpMail.value);
   
     const handleClick = () => {
-      window.open(`https://wa.me/${phoneNumber}?text= Name : ${name}, Email : ${email}, Message : ${message}`, '_blank');
+        window.open(`https://wa.me/${phoneNumber}?text= Name : ${name}, Email : ${email}, Message : ${message}`, '_blank');
     };
 
 
